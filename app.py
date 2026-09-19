4dc3e1fbcfde8a5f67e6348cc6f47ca2511558bc
rudrakrishna13062004-cmyk <rudrakrishna13062004@gmail.com>
2026-09-19 18:23:01 +0530

    Fix file paths for model and vectorizer loading

--- app.py
+++ app.py
@@ -19,9 +19,9 @@ app = FastAPI(
 
 # Load model + vectorizer once at startup
 try:
-    with open("/sentiment_model.pkl", "rb") as f:
+    with open("sentiment_model.pkl", "rb") as f:
         model = pickle.load(f)
-    with open("/vectorizer.pkl", "rb") as f:
+    with open("vectorizer.pkl", "rb") as f:
         vectorizer = pickle.load(f)
 except FileNotFoundError:
     model, vectorizer = None, None
